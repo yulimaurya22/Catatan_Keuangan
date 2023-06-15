2d59236a0cf7c3a6420cd0f462c2234be41a3866
yulimaurya22 <yulimaurya@gmail.com>
2023-06-15 18:30:04 +0700

    layout dan splash screen

--- app/src/main/java/com/yulia/catatankeuangan/MainActivity.java
+++ app/src/main/java/com/yulia/catatankeuangan/MainActivity.java
@@ -2,13 +2,33 @@ package com.yulia.catatankeuangan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+
+        int SPLASH_TIME=2000;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,catatan.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        }, SPLASH_TIME);
     }
+
 }
---
+++ app/src/main/java/com/yulia/catatankeuangan/catatan.java
@@ -0,0 +1,14 @@
+package com.yulia.catatankeuangan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class catatan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catatan);
+    }
+}
