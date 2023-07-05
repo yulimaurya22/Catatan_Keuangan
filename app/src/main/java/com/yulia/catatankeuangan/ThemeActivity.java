0076ba28fd4ae8f3a9bbc024621bf37fd37799f4
MAY RISKA <mayriskaputri789@gmail.com>
2023-07-05 18:49:49 +0700

    menambahkan script di ThemeActivity.java

--- app/src/main/java/com/yulia/catatankeuangan/ThemeActivity.java
+++ app/src/main/java/com/yulia/catatankeuangan/ThemeActivity.java
@@ -1,14 +1,66 @@
 package com.yulia.catatankeuangan;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class ThemeActivity extends AppCompatActivity {
+
+    ActionBar actionBar;
+    private RadioGroup radioGroup;
+    private RadioButton light, dark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme);
+
+        actionBar = getSupportActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_UNSPECIFIED);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        radioGroup = findViewById(R.id.radioGroup);
+        light = findViewById(R.id.themeLight);
+        dark = findViewById(R.id.themeDark);
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO)
+            light.setChecked(true);
+        else if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
+            dark.setChecked(true);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int itemSelected) {
+                switch (itemSelected) {
+                    case R.id.themeLight:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        break;
+                    case R.id.themeDark:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        break;
+                }
+
+            }
+        });
     }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {super.onBackPressed();}
 }
